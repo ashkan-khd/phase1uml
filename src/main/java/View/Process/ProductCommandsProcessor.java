2c3cf19d6e09b4cffb3d24348543d653d6cae0d1
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2020-04-12 19:08:04 +0430

    finishing product commands

---
+++ src/main/java/View/Process/ProductCommandsProcessor.java
@@ -0,0 +1,19 @@
+package View.Process;
+
+public class ProductCommandsProcessor extends CommandsProcessor {
+    public void processProductCommands(){}
+
+    private void digest(){}
+
+    private void addToCart(){}
+
+    private void selectSeller(String sellerUserName){}
+
+    private void attributes(){}
+
+    private void compareTo(String productId){}
+
+    private void comments(){}
+
+    private void addComment(){}
+}
