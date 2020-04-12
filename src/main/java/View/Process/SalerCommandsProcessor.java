9a1f864c9cd5de571ceeb3e9871e09393b5ec557
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2020-04-12 18:29:41 +0430

    finishing salers commands

--- src/main/java/View/Process/SalerCommandsProcessor.java
+++ src/main/java/View/Process/SalerCommandsProcessor.java
@@ -1,5 +1,33 @@
 package View.Process;
 
 public class SalerCommandsProcessor extends UserCommandsProcessor {
+    public void processSalersCommands(){}
 
+    private void viewCompanyInformation(){}
+
+    private void viewSalesHistory(){}
+
+    private void manageProducts(){}
+
+    private void viewProduct(String productId){}
+
+    private void viewProductBuyers(String productId){}
+
+    private void editProduct(String productId) {}
+
+    private void addProduct(){}
+
+    private void removeProduct(String productId){}
+
+    private void showCategories(){}
+
+    private void viewOffs(){}
+
+    private void viewOff(String offId){}
+
+    private void editOff(String offId){}
+
+    private void addOff(){}
+
+    private void viewBalance(){}
 }
