153772296a573ab98aa237fe63cf617ab6d335bc
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2020-04-12 18:18:49 +0430

    finishing managers commands

--- src/main/java/View/Process/ManagerCommandsProcessor.java
+++ src/main/java/View/Process/ManagerCommandsProcessor.java
@@ -1,5 +1,43 @@
 package View.Process;
 
 public class ManagerCommandsProcessor extends UserCommandsProcessor {
+    public void processManagerCommands(){}
 
+    private void manageUsers(){}
+
+    private void viewUser(String userName){}
+
+    private void deleteUser(String userName){}
+
+    private void createManagerProfile(){}
+
+    private void manageAllPrducts(){}
+
+    private void removeProduct(String productId){}
+
+    private void createDiscountCode(){}
+
+    private void viewDiscountCodes(){}
+
+    private void viewDiscountCode(String code){}
+
+    private void editDiscountCode(String code){}
+
+    private void removeDiscountCode(String code){}
+
+    private void manageRequests(){}
+
+    private void giveRequestDetails(String requestId){}
+
+    private void acceptRequest(String requestId){}
+
+    private void declineRequest(String requestId){}
+
+    private void manageCategories(){}
+
+    private void editCategory(String categoryName){}
+
+    private void addCategory(String categoryName){}
+
+    private void removeCategory(String categoryName){}
 }
